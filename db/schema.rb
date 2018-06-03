3e280bbef5d0e171435e7b078a998a5dc0531e14
君島克弥 <K.Kimishima123@gmail.com>
2018-06-03 15:34:45 +0900

    bin/rails g model tag name:string bin/rails g model article_tag article:references tag:referenc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_03_054103) do
+ActiveRecord::Schema.define(version: 2018_06_03_062522) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_06_03_054103) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
