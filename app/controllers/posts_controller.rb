d9d860a0472322cbfcb378019236d52c2a32bc2e
君島克弥 <K.Kimishima123@gmail.com>
2018-06-07 22:40:09 +0900

    記事の詳細表示機能追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,15 @@
 class PostsController < ApplicationController
+  before_action :check_page
+
   def show
+    @post = Article.find(params[:id])
+  end
+
+  private
+
+  def check_page
+    if params[:id].to_i < 0
+      redirect_to :root
+    end
   end
 end
