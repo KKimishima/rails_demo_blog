cf261736fc365987a996c0a8f77b3b466114f96a
君島克弥 <K.Kimishima123@gmail.com>
2018-06-12 22:33:47 +0900

    セッション管理機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   # クロスサイトリクエストフォージェリ対策
   protect_from_forgery with: :exception, prepend: true
+  include SessionsHelper
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    log_out
+    redirect_to login_url
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      reset_session
-      session[:user_id] = user.id
+      log_in(user)
       redirect_to :root
     else
-      render 'new'
+      flash.now[:danger] = "Emailかパスワードが間違えています"
+      render 'sessions/new'
     end
   end
 end
