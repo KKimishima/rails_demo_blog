690d901ab66fdaf34223d681b6f19f0255b2e31a
君島克弥 <K.Kimishima123@gmail.com>
2018-07-29 13:06:40 +0900

    公開前位準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
+  resources :about, only:[:index]
   resources :articles
   resources :archives, only: [:show]
   resources :tags
